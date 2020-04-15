8b9c325f205b9366f778b10b37fa5113360d4463
ramkasula2019 <ram.kasula@cotiviti.com>
2020-04-15 09:02:31 +0545

    Part-4 Generic View commited

--- polls/views.py
+++ polls/views.py
@@ -1,29 +1,27 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from polls.models import Question, Choice
-from django.template import loader
+from django.http import HttpResponseRedirect
+from django.views import generic
 from django.urls import reverse
 
+from polls.models import Question, Choice
+
 # Create your views here.
-def index (requests):
-    latest_question_list = Question.objects.order_by('-pub_date') [:5]
-    #output = ','.join(q.question_text for q in latest_question_list)
-    #return HttpResponse(output)
-    #template = loader.get_template('polls/index.html')
-    context = {'latest_question_list':latest_question_list}
-    #return HttpResponse(template.render(context, requests))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    return render(requests, 'polls/index.html', context)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date') [:5]
 
 
-def detail(requests, question_id):
-    question = get_object_or_404(Question, pk = question_id)
-    return render(requests, 'polls/detail.html', {'question':question})
+class DetailView(generic.DetailView):
+    template_name = 'polls/detail.html'
+    model = Question
 
-def results(requests, question_id):
-    question = get_object_or_404(Question, pk = question_id)
-    return render(requests,'polls/results.html', {'question':question})
-
+class ResultsView(generic.DetailView):
+    template_name = 'polls/results.html'
+    model = Question
+    
 def vote(requests, question_id):
     question = get_object_or_404(Question, pk = question_id)
     try:
